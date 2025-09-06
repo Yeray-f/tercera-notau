7a803ee40a4c2cfec8c7fc1e32fee7168693f715
Yeray-f <yfalvarezp@ufpso.edu.co>
2025-09-05 20:51:40 -0500

    Add files via upload

---
+++ src/logicos.java
@@ -0,0 +1,12 @@
+package Ejemplos;
+public class logicos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 20;
+
+        System.out.println("a > b && b < c: " + (a > b && b < c)); // true
+        System.out.println("a < b || b > c: " + (a < b || b > c)); // false
+        System.out.println("!(a > b): " + !(a > b)); // false
+    }
+}
