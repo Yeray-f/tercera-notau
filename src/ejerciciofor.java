e7f33671a4c647e7759ebd7dbc473afd12817718
Yeray-f <yfalvarezp@ufpso.edu.co>
2025-09-06 07:18:43 -0500

    Add files via upload

---
+++ src/ejerciciofor.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ejerciciofor {
+   int num1 = 1;
+   int num2 = 5;
+   int num3 = 1;
+   int nummax;
+   for(int i=0; i<4; i++){
+if (num1 > num2 && num1>num3) {
+        nummax = num1    
+    }
+else if(num2>num1 && num2>num3){
+        nummax = 2;
+    }
+else{
+        nummax = num3
+    }
+   } 
+}
